ad99716db56baa89d092083be6df40611c015515
takeyuki197 <takeyuki197@gmail.com>
2018-07-09 23:14:51 +0900

    Add initial files

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'How to 結婚' });
+});
+
+module.exports = router;
